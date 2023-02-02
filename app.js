0f348a6e597610167df429a21f518b26ce238c34
Matheus Pereira <empregos97@gmail.com>
2023-02-02 16:03:35 -0300

    fix:readme update

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express()
+const port = 5555; 
+
+
+app.get('/' , (req,res) => {
+  res.send('Hello World')
+})
+
+app.listen(port , (req,res) => {
+  console.log('Running')
+})
+
